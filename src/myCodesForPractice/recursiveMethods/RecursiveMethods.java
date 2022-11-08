ee799748ac35b428b2b7fa9da00ad23e2290c972
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2022-11-08 22:56:04 +0300

    reverseArray() method added.

--- src/myCodesForPractice/recursiveMethods/RecursiveMethods.java
+++ src/myCodesForPractice/recursiveMethods/RecursiveMethods.java
@@ -2,11 +2,20 @@ package myCodesForPractice.recursiveMethods;
 
 public class RecursiveMethods {
     public static void main(String[] args) {
+        int[] array = {2,3,4,5,6,7,8,9,15,19};
+    /*
         //Linear Sum
-        int[] array = {1,2,3,4,5,6,7,8,9};
+
         System.out.println("Sum of array is = "+linearSum(array,array.length-1));
+    */
 
-
+    /*
+        //reverseArray
+        int[] array2=reverseArray(array,0,array.length-1);
+        for (int i = 0; i < array2.length; i++) {
+            System.out.print(array2[i]+",");
+        }
+    */
 
     }
 
@@ -25,7 +34,16 @@ public class RecursiveMethods {
 
     }
 
-
+    public static int[] reverseArray(int[] data,int firstIndex,int lastIndex){
+        if (firstIndex >=lastIndex) {
+            return data;
+        }else {
+            int temp= data[firstIndex];
+            data[firstIndex]=data[lastIndex];
+            data[lastIndex]=temp;
+            return reverseArray(data,firstIndex+1,lastIndex-1);
+        }
+    }
 
 
 
